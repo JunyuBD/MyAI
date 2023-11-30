a4373cd77d5578b3f7bc7739015c5fb431a6525e
junyu.wang <junyu.wang@bytedance.com>
2023-11-29 20:50:58 -0800

    add

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
+import json
+from urllib import request
+
 from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
 
+@app.route("/bot/callback", methods=["POST"])
+def bot_callback():
+    print(request)
+    if 'challenge' in request.json:
+        return json.dumps({
+            "challenge": request.json['challenge']
+    })
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+    print(request.json)
+    print(request.json['event']['sender']['sender_id']['open_id'])
+    user_open_id = request.json['event']['sender']['sender_id']['open_id']
+    default_respond = json.dumps({
+        "success": "cool"
+    })
