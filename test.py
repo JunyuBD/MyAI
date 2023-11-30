aeba7593cfa308c262a7849ff6035911ac86a855
junyu.wang <junyu.wang@bytedance.com>
2023-11-29 22:53:27 -0800

    add assistant

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from assistant import *
 if __name__ == '__main__':
     assistant = Assistant()
 
-    print("success")
+    print("success??")
     assistant.add_user_message_to_thread("just give me the raw help me to create a lark doc about the content of lark platform, help me to record: 后续如果研发有一些不需要查数据库的非紧急数据，也可以申请一下上面的数据表权限进行查询～目前海外数据库需要每天申请临时权限真的太")
     run = assistant.get_run()
     if run is None:
