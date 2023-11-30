17a3cf2745decdf7b600a35a168a5409ee983907
junyu.wang <junyu.wang@bytedance.com>
2023-11-29 23:43:59 -0800

    filter out dup msg

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from urllib import request
 from flask import Flask, request, json
-import threading
+
 from assistant import *
 from utils import *
 
@@ -9,34 +9,7 @@ app = Flask(__name__)
 ASSISTANT_BOT_OPEN_ID = "ou_376caaa69595a9fa72425363c34dfc91"
 
 assistant_poll = {}
-
-def handle_time_consuming_task(user_msg_with_open_id, user_open_id, message_id):
-    default_respond = json.dumps({
-        "success": "cool"
-    })
-    print("user_open_id is {}, msg {}, msg_id is {}".format(user_open_id, user_msg_with_open_id, message_id))
-    if user_open_id not in assistant_poll:
-        assistant = Assistant()
-        assistant_poll[user_open_id] = assistant
-    else:
-        assistant = assistant_poll[user_open_id]
-
-    assistant.add_user_message_to_thread(user_msg_with_open_id)
-
-    run = assistant.get_run()
-    if run is None:
-        print("run is None. ")
-        return default_respond
-
-    assistant.execute_run(run)
-
-    response = assistant.get_latest_assistant_message()
-
-    print(response)
-
-    reply_to_user(message_id, response)
-    print("reply to user success")
-
+message_map = {}
 @app.route("/bot/callback", methods=["POST", "GET" ])
 def bot_callback():
     print("request is {}".format(request))
@@ -57,15 +30,37 @@ def bot_callback():
         print('bot not mentioned in the group')
         return default_respond
     message_id = request.json['event']['message']['message_id']
+    if message_id in message_map:
+        print("message already handled")
+        return default_respond
+
+    message_map[message_id] = True
     user_msg = get_msg(request.json['event'])
     user_msg_with_open_id = replace_user_with_id(request.json['event'], user_msg)
     assistant = None
     print(f"user_msg_with_open_id {user_msg_with_open_id}")
-    # Start the time-consuming task in a background thread
-    thread = threading.Thread(target=handle_time_consuming_task, args=(user_msg_with_open_id, user_open_id, message_id,))
-    thread.start()
+    if user_open_id not in assistant_poll:
+        assistant = Assistant()
+        assistant_poll[user_open_id] = assistant
+    else:
+        assistant = assistant_poll[user_open_id]
 
+    assistant.add_user_message_to_thread(user_msg_with_open_id)
 
+    run = assistant.get_run()
+    if run is None:
+        print("run is None. ")
+        return default_respond
+
+    assistant.execute_run(run)
+
+    response = assistant.get_latest_assistant_message()
+
+    print(response)
+
+    reply_to_user(message_id, response)
+
+    print("reply to user success")
     return default_respond
 
 
